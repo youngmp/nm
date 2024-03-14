bc7545e33243fde3b42900172c876cc955866d90
youngmp <ympark1988@gmail.com>
2024-03-14 10:35:46 -0400

    moved library functions for specific function calls

---
+++ lib/functions.py
@@ -0,0 +1,31 @@
+"""
+library for various functions
+"""
+
+import numpy as np
+
+
+def p(x,s=1):
+    """
+    Unnormalized periodized Gaussian.
+    """
+    
+    x = np.mod(x,2*np.pi)
+    tot = 0
+    for i in range(-10,10+1):
+        tot -= np.exp(-(((x+i*2*np.pi))/s)**2)
+    return tot
+
+x_temp,dx_temp = np.linspace(-np.pi,np.pi,1000,retstep=True)
+pbar = np.sum(p(x_temp))*dx_temp/(2*np.pi)
+
+def g1(x,s=1):
+    """
+    Periodized Gaussian with subtracted mean.
+    """
+    
+    x = np.mod(x,2*np.pi)
+    tot = 0
+    for i in range(-10,10+1):
+        tot -= np.exp(-(((x+i*2*np.pi))/s)**2)
+    return tot - pbar
